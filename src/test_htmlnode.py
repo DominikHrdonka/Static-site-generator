a7f7fcad835787358a6133bea1142c041abb7d0b
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-04-09 14:38:43 +0200

    add another tests of props_to_html

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -7,6 +7,13 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props={"href":"www.ahoj.cz", "target":"_blank"})
         self.assertEqual(node.props_to_html(), ' href="www.ahoj.cz" target="_blank"')
 
+    def test_props2(self):
+        node = HTMLNode(props={"href":"boot.dev", "target":"parent"})
+        self.assertEqual(node.props_to_html(), ' href="boot.dev" target="parent"')
+    
+    def test_props3(self):
+        node = HTMLNode(props={"href":"www.gotit.com", "target":"top"})
+        self.assertNotEqual(node.props_to_html(), 'href="www.gotit.com" target="_top"')
 
 if __name__ == "__main__":
     unittest.main()
