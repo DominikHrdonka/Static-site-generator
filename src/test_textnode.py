cd5c8ecbcafbd30c127196686d71729fa52696b4
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-04-09 13:42:27 +0200

    add other test methods and run tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,27 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+    
+    def test_eq_type(self):
+        node = TextNode("This is a test", "italic", None)
+        node2 = TextNode("This is a test", "bold")
+        self.assertNotEqual(node, node2)
+    
+    def test_eq_text(self):
+        node = TextNode("Some node", "bold", None)
+        node2 = TextNode("This is a test", "bold")
+        self.assertNotEqual(node, node2)
+    
+    def test_eq_url(self):
+        node = TextNode("This is a test", "bold", "http://facebook.com")
+        node2 = TextNode("This is a test", "bold", "http://facebook.com")
+        self.assertEqual(node, node2)
+    
+    def test_repr(self):
+        node = TextNode("This is a text node", "text", "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 
 if __name__ == "__main__":
