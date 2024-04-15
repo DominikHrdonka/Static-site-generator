4a39279ea114a0882794f5732a391cd2569b2673
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-04-15 16:10:15 +0200

    create ParentNode class and its to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,15 +18,30 @@ class HTMLNode:
         return f"{self}: {self.tag}, {self.value}, {self.children}, {self.props}"
     
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, children = None, props=None):
-        super().__init__(tag, value, children, props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
         if self.value == None:
             raise ValueError("Value property is required!")
 
     def to_html(self):
-    
         if self.tag == None:
             return f"{self.props_to_html()} {self.value}"
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-            
+
+class ParentNode(HTMLNode):
+    def __init__(self, children, tag=None, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("You must provide the tag attribute.")
+        elif not self.children:
+            raise ValueError("Children attribute must be included.")
+        else:
+            def concatenate():
+                string = ""
+                for child in self.children:
+                    string += f"<{child.tag}>{child.value}</{child.tag}>"
+                return f"<{self.tag}>{string}</{self.tag}>"
+            return concatenate()
