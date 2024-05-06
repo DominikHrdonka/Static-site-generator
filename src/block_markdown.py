0c4734b66ce524767bc9ade850fa80f22d3b7f50
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-06 08:28:28 +0200

    add markdown_to_blocks function and tests

---
+++ src/block_markdown.py
@@ -0,0 +1,9 @@
+
+def markdown_to_blocks(markdown):
+    block_strings = []
+    split_blocks = markdown.split("\n\n")
+    for block in split_blocks:
+        if block == "":
+            continue
+        block_strings.append(block.strip(" "))
+    return block_strings
---
+++ src/test_block_markdown.py
@@ -0,0 +1,19 @@
+import unittest
+
+from block_markdown import markdown_to_blocks
+
+class TestBlockMarkdown(unittest.TestCase):
+
+    def test_block_markdown(self):
+        markdown = "This is **bolded** paragraph\n\nThis is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line\n\n* This is a list\n* with items"
+        expected_output = ["This is **bolded** paragraph", "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line", "* This is a list\n* with items"]
+        self.assertEqual(markdown_to_blocks(markdown), expected_output)
+    
+    def test_block_markdown_empty(self):
+        markdown = ""
+        expected_output = []
+        self.assertEqual(markdown_to_blocks(markdown), expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
