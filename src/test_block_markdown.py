8b111d2f57847b2be9e74556144a423da295468f
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-06 10:05:00 +0200

    add block_type_paragraph test

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -62,6 +62,10 @@ This is the same paragraph on a new line
         block = "## This is a heading"
         self.assertEqual(block_to_block_type(block), block_type_heading)
 
+    def test_block_to_block_type_heading_2(self):
+        block = "#### This is a heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+
     def test_block_to_block_type_code(self):
         block = "```This is code```"
         self.assertEqual(block_to_block_type(block), block_type_code)
@@ -78,6 +82,9 @@ This is the same paragraph on a new line
         block = "1. This is ordered list line\n2. This is another ordered list line\n3. This is last ordered list line"
         self.assertEqual(block_to_block_type(block), block_type_ordered_list)
 
+    def test_block_to_block_type_paragraph(self):
+        block = ">This is paragraph line\nThis is also paragraph line"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
 
 if __name__ == "__main__":
     unittest.main()
